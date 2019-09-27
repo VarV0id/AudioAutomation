8d0aac74d4280134c03f8a99f5d8113f138e0e96
Varv0id <jairocampana10001@gmail.com>
2019-09-27 17:35:44 -0500

    [Code]Proyecto limpiado

--- Backend/app.js
+++ Backend/app.js
@@ -1,30 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require('cors');
 const bodyParser = require('body-parser');
- 
 var transcriptRouter = require('./routes/transcriptRouter');
 
 //Inicializa el framework
 var app = express();
+//Se activa cors
 app.use(cors());
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-//activa cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT  , DELETE');
-  next();
-});
-
-//Se crea la ruta para consumir el servicio rest de trasncripcion.
-
+//Se crea la ruta para consumir el sesrvicio rest de trasncripcion.
 transcriptRouter(app)
 //Se escucha por el puerto 3000
 app.listen(3000, () => {
@@ -34,7 +20,6 @@ app.listen(3000, () => {
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
